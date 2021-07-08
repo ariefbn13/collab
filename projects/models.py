d2611a6d88103c96d2f9dcfd11c58e20d8afa380
ariefbn13 <ariefbn@yahoo.com>
2021-07-08 13:46:07 +0700

    push

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path='projects/static/img/')
+    image = models.ImageField(upload_to='projects/img')
 def __str__(self):
     return self.title
 class Meta:
