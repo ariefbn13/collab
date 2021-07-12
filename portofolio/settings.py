4e1b67359fb7c752fbe6167926b7cad38fd95ee8
ariefbn13 <ariefbn@yahoo.com>
2021-07-12 11:21:15 +0700

    push

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -136,9 +136,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media Path
-MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
